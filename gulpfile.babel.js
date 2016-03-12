0ccd68139c0e4a7f538d79bf391868d7e9e13bd1
Sebastian Herber <sh@zaplab.at>
2016-03-12 15:18:57 +0100

    Adds travis config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,15 @@ import eventStream from 'event-stream';
 import gutil from 'gulp-util';
 import webpack from 'webpack';
 
+function onWarning(error) {
+    gutil.log(error);
+}
+
+function onError(error) {
+    gutil.log(error);
+    process.exit(1);
+}
+
 gulp.task('clean', gulpCallback => {
     del([
         'dist',
@@ -20,9 +29,7 @@ gulp.task('eslint', () => {
             configFile: 'tests/.eslintrc',
         }))
         .pipe(eslint.format())
-        .on('error', error => {
-            console.error('' + error);
-        });
+        .on('error', onWarning);
 });
 
 // for easier debugging of the generated spec bundle
@@ -48,12 +55,16 @@ gulp.task('specs:debug', gulpCallback => {
 });
 
 gulp.task('specs', gulpCallback => {
-    const KarmaServer = require('karma').Server;
+    const karmaServer = require('karma').Server;
 
-    new KarmaServer.start({
+    karmaServer.start({
         configFile: __dirname + '/karma.conf.js',
         singleRun: true,
-    }, () => {
+    }, karmaExitCode => {
+        if (karmaExitCode !== 0) {
+            process.exit(1);
+        }
+
         gulpCallback();
     });
 });
@@ -96,9 +107,7 @@ gulp.task('js', [
     return gulp.src('src/*.js')
         .pipe(babel())
         .pipe(gulp.dest('dist'))
-        .on('error', error => {
-            console.error('' + error);
-        });
+        .on('error', onError);
 });
 
 gulp.task('default', [
